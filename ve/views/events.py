a3f2d24fbe79d9fe57a21942b203b6007b2b67fd
Georges Labreche <georges@opendatakosovo.org>
2015-02-01 17:20:53 -0800

    Added events to landing page

--- ve/views/events.py
+++ ve/views/events.py
@@ -7,7 +7,7 @@ import json
 class Events(View):
     def dispatch_request(self):
         api_base_url = utils.get_api_url()
-        url = "%s/display/events" %(api_base_url)
+        url = "%s/display/events" % (api_base_url)
 
         events = urlopen(url).read()
         events_json = json.loads(events)
--- ve/views/index.py
+++ ve/views/index.py
@@ -1,6 +1,16 @@
 from flask import render_template
 from flask.views import View
+from urllib2 import urlopen
+from ve import utils
+import json
 
 class Index(View):
     def dispatch_request(self):
-        return render_template("index.html")
+    	api_base_url = utils.get_api_url()
+        url = "%s/display/events" % (api_base_url)
+
+        events_str = urlopen(url).read()
+
+        events = json.loads(events_str)
+
+        return render_template("index.html", events=events)
